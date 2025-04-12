071ccfc17e3f220bebbb6570fd43af7b723e2cda
nicohzk <nicohzk05@gmail.com>
2025-04-12 10:55:33 +0700

    menyelesaikan program dengan runtime configuration

---
+++ Tpmodul8_103022300127/CovidConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Tpmodul8_103022300127
+{
+    class CovidConfig
+    {
+        public string satuan_suhu { get; set; }
+        public int batas_hari_deman { get; set; }
+        public string pesan_ditolak { get; set; }
+        public string pesan_diterima { get; set; }
+
+        readonly string filePath = Path.GetFullPath("../../../../datas/covid_config.json");
+        public void ReadJSON() {
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                CovidConfig x = JsonSerializer.Deserialize<CovidConfig>(json); 
+                satuan_suhu = x.satuan_suhu;
+                batas_hari_deman = x.batas_hari_deman;
+                pesan_ditolak = x.pesan_ditolak;
+                pesan_diterima = x.pesan_diterima;
+            }
+            ;
+        }
+        public void UbahSatuan()
+        {
+            satuan_suhu = (satuan_suhu == "celcius") ? "fahrenheit" : "celcius";
+        }
+    }
+
+}
--- Tpmodul8_103022300127/Program.cs
+++ Tpmodul8_103022300127/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Tpmodul8_103022300127;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CovidConfig x = new CovidConfig();
+        x.ReadJSON();
+        x.UbahSatuan();
+        Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam nilai {x.satuan_suhu}");
+        double suhu = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine($"Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala deman?");
+        int hari = Convert.ToInt32(Console.ReadLine());
+
+        if (x.satuan_suhu == "celcius")
+        {
+            if (suhu < 36.5 || suhu > 37.5 || hari < x.batas_hari_deman)
+            {
+                Console.WriteLine(x.pesan_ditolak);
+            }
+            else
+            {
+                Console.WriteLine(x.pesan_diterima);
+            }
+        }
+        else if (x.satuan_suhu == "fahrenheit") {
+            if (suhu < 97.7 || suhu > 99.5 || hari < x.batas_hari_deman)
+            {
+                Console.WriteLine(x.pesan_ditolak);
+            }
+            else
+            {
+                Console.WriteLine(x.pesan_diterima);
+            }
+        }
+    }
+}
